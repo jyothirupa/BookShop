72b05cb21bfb27de107e9b26e33772ae13e4f8d1
Jyothi <Jyothi@LAPTOP-IAH79NFD>
2017-08-01 14:35:43 +0530

    registration completed

--- BookShopFrontEnd/src/main/java/com/bookshop/controller/HomeController.java
+++ BookShopFrontEnd/src/main/java/com/bookshop/controller/HomeController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@RequestMapping({"/","/index"})
-	public String hello(){		
+
+	@RequestMapping("/")
+	public String hello() {
 		return "index";
 	}
 
--- BookShopFrontEnd/target/BookShopFrontEnd/WEB-INF/view/index.jsp
+++ BookShopFrontEnd/target/BookShopFrontEnd/WEB-INF/view/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
-Hello world
+	<a href="signIn">Sign In</a><br/><br/>
+	
+	<a href="register">Sign Up</a>
 </body>
 </html>
